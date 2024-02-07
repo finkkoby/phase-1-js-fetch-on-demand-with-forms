952736ac328149af67bb516b8cd89b307bb18961
Koby Fink <finkkoby@icloud.com>
2024-02-07 17:21:46 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", function(event){
+        event.preventDefault();
+        let input = event.target.children[1];
+        fetch(`http://localhost:3000/movies/${input.value}`)
+            .then(res => res.json())
+            .then(data => {
+                const title = document.querySelector("section#movieDetails h4");
+                const summary = document.querySelector("section#movieDetails p");
+                title.innerText = data.title;
+                summary.innerText = data.summary;
+            })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
